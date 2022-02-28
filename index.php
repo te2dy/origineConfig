820bee22d1da7efbf95dda64c18fb60cd7d30db2
te2dy <52241628+te2dy@users.noreply.github.com>
2022-02-28 22:30:33 +0100

    Améliorations diverses

--- index.php
+++ index.php
@@ -380,16 +380,13 @@ if (!empty($_POST)) {
     }
 
     // Header and footer alignment
-    if ($header_footer_align === 'left') {
-      $css_array['#site-header']['text-align'] = 'left';
-      $css_array['#site-footer']['text-align'] = 'left';
-    } else {
+    if ($header_footer_align === 'center') {
       $css_array['#site-header']['text-align'] = 'center';
       $css_array['#site-footer']['text-align'] = 'center';
-    }
 
-    $css       .= origineConfigArrayToCSS($css_array);
-    $css_array  = [];
+      $css       .= origineConfigArrayToCSS($css_array);
+      $css_array  = [];
+    }
 
     // Logo
     if ($logo_url) {
@@ -431,16 +428,17 @@ if (!empty($_POST)) {
         $css_array['.post-list-standard .post-link']['display'] = 'block';
         $css_array['.post-list-standard .post-meta']['margin-bottom'] = '.25em';
         $css_array['.post-list-standard .post-title']['font-size'] = '1.1em';
+        $css_array['.post-list-standard .post-selected-label']['border-left'] = 'none';
         $css_array['.post-list-standard .post-selected-label']['margin-left'] = '-1rem';
         $css_array['.post-list-standard .post-selected-label']['margin-bottom'] = '.5rem';
         $css_array['.post-list-standard .post-list-selected-content']['border-left'] = '.063rem solid var(--color-border)';
         $css_array['.post-list-standard .post-list-selected-content']['padding-left'] = '1rem';
-        $css_array['.post-list-standard .post-list-comment']['font-size'] = '.9em';
-        $css_array['.post-list-standard .post-list-comment']['margin-top'] = '.25rem';
+        $css_array['.post-list-standard .label']['margin-bottom'] = '.5rem';
+        $css_array['.post-list-standard .post-list-comment']['display'] = 'inline-block';
+        $css_array['.post-list-standard .post-list-comment']['margin-left'] = '.25em';
         break;
 
       case 'short':
-        $css_array['.post-list-short .post-link']['display'] = 'inline-block';
         $css_array['.post-list-short .post-selected-label']['display'] = 'inline-block';
         $css_array['.post-list-short .post-meta, .post-list-short .post-title']['font-size'] = '1em';
         $css_array['.post-list-short .post-title']['display'] = 'inline';
@@ -537,7 +535,11 @@ if (!empty($_POST)) {
 
       $css_array['.footer-social-links a:active .footer-social-links-icon-container, .footer-social-links a:focus .footer-social-links-icon-container, .footer-social-links a:hover .footer-social-links-icon-container']['background-color'] = 'var(--color-input-background-hover)';
 
-      $css_array['.footer-social-links a:active .footer-social-links-icon, .footer-social-links a:focus .footer-social-links-icon, .footer-social-links a:hover .footer-social-links-icon']['fill']       = 'var(--color-input-text-hover)';
+      $css_array['.footer-social-links a']['border-bottom'] = 'none';
+
+      $css_array['.footer-social-links a:active, .footer-social-links a:focus, .footer-social-links a:hover']['border-bottom'] = 'none';
+
+      $css_array['.footer-social-links a:active .footer-social-links-icon, .footer-social-links a:focus .footer-social-links-icon, .footer-social-links a:hover .footer-social-links-icon']['fill']          = 'var(--color-input-text-hover)';
 
       if ($css_transition === true) {
         $css_array['.footer-social-links-icon-container']['transition'] = 'all .2s ease-in-out';
