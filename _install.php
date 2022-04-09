bb2bc0fc62c96d86a1972b0dda1da1409f61367b
te2dy <52241628+te2dy@users.noreply.github.com>
2022-04-09 23:40:37 +0200

    Suppression paramètres inutilisés (résout #13)

--- _install.php
+++ _install.php
@@ -20,9 +20,52 @@ if (version_compare($old_version, $new_version, '>=')) {
 try {
   $core->blog->settings->addNamespace('origineConfig');
 
-  // Default settings.
+  // Old settings to delete from the database on the next version.
+  $settings_to_drop = [
+    'activation',
+    'color_scheme',
+    'color_link',
+    'css_transition',
+    'meta_generator',
+    'header_footer_align',
+    'post_list_type',
+    'sidebar_enabled',
+    'footer_enabled',
+    'logo_url',
+    'logo_url_2x',
+    'logo_type',
+    'content_font_family',
+    'content_font_size',
+    'content_text_align',
+    'content_hyphens',
+    'post_author_name',
+    'post_list_author_name',
+    'post_list_comments',
+    'comment_links',
+    'post_email_author',
+    'footer_credits',
+    'social_links_diaspora',
+    'social_links_discord',
+    'social_links_facebook',
+    'social_links_github',
+    'social_links_mastodon',
+    'social_links_signal',
+    'social_links_tiktok',
+    'social_links_twitter',
+    'social_links_whatsapp',
+    'origine_styles',
+  ];
+
+  // Deletes old settings.
+  foreach($settings_to_drop as $setting_id) {
+    if ($core->blog->settings->origineConfig->$setting_id) {
+      $core->blog->settings->origineConfig->dropEvery($setting_id, true);
+    }
+  }
+
+  // Default settings to define in the database.
   $origine_settings = [
-    'activation'           => false,
+    'activation'            => false,
     'global_color_scheme'   => 'system',
     'global_color_link'     => 'red',
     'global_css_transition' => false,
