3f8ad1027b57f68b4df03232b97c2fd4fd623a22
te2dy <52241628+te2dy@users.noreply.github.com>
2022-04-10 00:24:16 +0200

    Suppression paramètres désuets de base de données

--- _install.php
+++ _install.php
@@ -22,14 +22,14 @@ try {
 
   // Old settings to delete from the database on the next version.
   $settings_to_drop = [
-    'activation',
+    'global_activation',
     'color_scheme',
-    'color_link',
+    'link_color',
     'css_transition',
     'meta_generator',
     'header_footer_align',
     'post_list_type',
-    'sidebar_enabled',
+    'widgets_enabled',
     'footer_enabled',
     'logo_url',
     'logo_url_2x',
@@ -54,53 +54,60 @@ try {
     'social_links_twitter',
     'social_links_whatsapp',
     'origine_styles',
+    'sidebar_enabled',
   ];
 
   // Deletes old settings.
   foreach($settings_to_drop as $setting_id) {
-    if ($core->blog->settings->origineConfig->$setting_id) {
-      $core->blog->settings->origineConfig->dropEvery($setting_id, true);
-    }
+    $core->blog->settings->origineConfig->dropEvery($setting_id, true);
   }
 
   // Default settings to define in the database.
   $origine_settings = [
-    'activation'            => false,
+    'activation' => false,
+
+    // Global
     'global_color_scheme'   => 'system',
     'global_color_link'     => 'red',
     'global_css_transition' => false,
     'global_meta_generator' => false,
 
-    'header_align'          => 'left',
-    'header_widgets_nav'    => true,
+    // Header
+    'header_align'       => 'left',
+    'header_widgets_nav' => true,
+    'header_logo_url'    => '',
+    'header_logo_url_2x' => '',
+    'header_logo_type'   => 'square',
 
-    'footer_align'          => 'left',
-    'post_list_type'        => 'standard',
-    'sidebar_enabled'       => true,
-    'footer_enabled'        => true,
-    'logo_url'              => '',
-    'logo_url_2x'           => '',
-    'logo_type'             => 'square',
-    'content_font_family'   => 'serif',
-    'content_font_size'     => 100,
-    'content_text_align'    => 'left',
-    'content_hyphens'       => 'disabled',
-    'post_author_name'      => 'disabled',
-    'post_list_author_name' => 0,
-    'post_list_comments'    => 0,
-    'comment_links'         => 1,
-    'post_email_author'     => 'disabled',
-    'share_link_twitter'    => false,
-    'footer_credits'        => true,
-    'social_links_diaspora' => '',
-    'social_links_discord'  => '',
-    'social_links_facebook' => '',
-    'social_links_github'   => '',
-    'social_links_mastodon' => '',
-    'social_links_signal'   => '',
-    'social_links_tiktok'   => '',
-    'social_links_twitter'  => '',
-    'social_links_whatsapp' => '',
+    // Content
+    'content_post_list_type'        => 'standard',
+    'content_font_family'           => 'serif',
+    'content_font_size'             => 100,
+    'content_text_align'            => 'left',
+    'content_hyphens'               => 'disabled',
+    'content_post_author_name'      => 'disabled',
+    'content_post_list_author_name' => 0,
+    'content_share_link_twitter'    => false,
+    'content_post_list_comments'    => 0,
+    'content_comment_links'         => 1,
+    'content_post_email_author'     => 'disabled',
+
+    // Widgets
+    'widgets_enabled' => true,
+
+    // Footer
+    'footer_enabled'               => true,
+    'footer_align'                 => 'left',
+    'footer_credits'               => true,
+    'footer_social_links_diaspora' => '',
+    'footer_social_links_discord'  => '',
+    'footer_social_links_facebook' => '',
+    'footer_social_links_github'   => '',
+    'footer_social_links_mastodon' => '',
+    'footer_social_links_signal'   => '',
+    'footer_social_links_tiktok'   => '',
+    'footer_social_links_twitter'  => '',
+    'footer_social_links_whatsapp' => '',
   ];
 
   $core->blog->settings->origineConfig->put('origine_settings', $origine_settings, 'array', 'All Origine settings', false, true);
