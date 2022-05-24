75c6da8b5c1d6fa4bcbf3a55c07185b3ba3551a7
te2dy <52241628+te2dy@users.noreply.github.com>
2022-05-24 19:20:18 +0200

    origineConfig 1.0.2-beta

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
   /* Name */        'origineConfig',
   /* Description */ 'A plugin to customize Origine theme',
   /* Author */      'Teddy',
-  /* Version */     '1.0.1-dev',
+  /* Version */     '1.0.2-beta',
   array(
     'permissions' => 'admin',
     'type'        => 'plugin',
