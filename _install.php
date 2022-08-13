5b443635a21fae12898cc3ca1d724e2719f08cbb
te2dy <teddyenligne@pm.me>
2022-08-13 22:30:50 +0200

    Correction espaces

--- _install.php
+++ _install.php
@@ -7,7 +7,7 @@
  */
 
 if (!defined('DC_CONTEXT_ADMIN')) {
-  return;
+    return;
 }
 
 include __DIR__ . '/settings.php';
@@ -16,52 +16,52 @@ $new_version = $core->plugins->moduleInfo('origineConfig', 'version');
 $old_version = $core->getVersion('origineConfig');
 
 if (version_compare($old_version, $new_version, '>=')) {
-  return;
+    return;
 }
 
 try {
-  $core->blog->settings->addNamespace('origineConfig');
+    $core->blog->settings->addNamespace('origineConfig');
 
-  /**
-   * Delete the old database entry which contained all the settings.
-   * 
-   * To remove after some releases.
-   */
-  $core->blog->settings->origineConfig->drop('origine_settings');
+    /**
+     * Delete the old database entry which contained all the settings.
+     *
+     * To remove after some releases.
+     */
+    $core->blog->settings->origineConfig->drop('origine_settings');
 
-  // Default settings to define in the database.
-  $oc_settings_default = origineConfigSettings::default_settings();
+    // Default settings to define in the database.
+    $oc_settings_default = origineConfigSettings::default_settings();
 
-  foreach($oc_settings_default as $setting_id => $setting_data) {
-    if ($setting_data['type'] === 'checkbox') {
-      $setting_type = 'boolean';
-    } else {
-      $setting_type = 'string';
+    foreach($oc_settings_default as $setting_id => $setting_data) {
+        if ($setting_data['type'] === 'checkbox') {
+            $setting_type = 'boolean';
+        } else {
+            $setting_type = 'string';
+        }
+
+        $core->blog->settings->origineConfig->put(
+            $setting_id,
+            $setting_data['default'],
+            $setting_type,
+            $setting_data['title'],
+            false,
+            true
+        );
     }
 
-    $core->blog->settings->origineConfig->put(
-      $setting_id,
-      $setting_data['default'],
-      $setting_type,
-      $setting_data['title'],
-      false,
-      true
-    );
-  }
+    // Removes unused settings if still exists.
+    $settings_to_unset = origineConfigSettings::settings_to_unset();
 
-  // Removes unused settings if still exists.
-  $settings_to_unset = origineConfigSettings::settings_to_unset();
+    foreach ($settings_to_unset as $setting_id) {
+        $core->blog->settings->origineConfig->dropEvery($setting_id, true);
+    }
 
-  foreach ($settings_to_unset as $setting_id) {
-    $core->blog->settings->origineConfig->dropEvery($setting_id, true);
-  }
+    // Sets the new version number of the plugin.
+    $core->setVersion('origineConfig', $new_version);
 
-  // Sets the new version number of the plugin.
-  $core->setVersion('origineConfig', $new_version);
-
-  return true;
+    return true;
 } catch (Exception $e) {
-  $core->error->add($e->getMessage());
+    $core->error->add($e->getMessage());
 }
 
 return false;
