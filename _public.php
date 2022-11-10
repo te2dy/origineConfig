29a18cb53bbf3467603d0f6cc9c9621549e89a8b
te2dy <teddyenligne@pm.me>
2022-11-10 18:27:52 +0100

    Ajout fichier JS et minification dans le public

--- _public.php
+++ _public.php
@@ -424,81 +424,12 @@ class origineConfig
         if (\dcCore::app()->blog->settings->origineConfig->active === true && \dcCore::app()->blog->settings->origineConfig->content_images_wide === true) {
                 $page_width_em = \dcCore::app()->blog->settings->origineConfig->global_page_width ? \dcCore::app()->blog->settings->origineConfig->global_page_width : 30;
                 ?>
-                    <script>
-                        var pageWidthEm = parseInt(<?php echo $page_width_em; ?>),
-                            imgWideWidthEm = pageWidthEm + 10,
-                            pageWidthPx = 0, // To set later.
-                            imgWideWidthPx = 0, // To set later.
-                            fontSizePx = 0; // To set later.
-
-                        /**
-                         * Gets the font size defined by the browser.
-                         *
-                         * @link https://brokul.dev/detecting-the-default-browser-font-size-in-javascript
-                         */
-                        const element = document.createElement('div');
-                        element.style.width = '1rem';
-                        element.style.display = 'none';
-                        document.body.append(element);
-
-                        const widthMatch = window.getComputedStyle(element).getPropertyValue('width').match(/\d+/);
-
-                        element.remove();
-
-                        // Sets the font size in px.
-                        if (widthMatch && widthMatch.length >= 1) {
-                            fontSizePx = parseInt(widthMatch[0]);
-                        }
-
-                        // If a font size is set, calculates page and image width in px.
-                        if (fontSizePx > 0) {
-                            pageWidthPx = pageWidthEm * fontSizePx;
-                            imgWideWidthPx = imgWideWidthEm * fontSizePx;
-                        }
-
-                        function getMeta(url, callback) {
-                            var img = new Image();
-                            img.src = url;
-                            img.onload = function() {
-                                callback(this.width, this.height);
-                            }
-                        }
-
-                        // Gets all images of the post.
-                        var img = document.getElementsByTagName("article")[0].getElementsByTagName("img"),
-                            i = 0;
-
-                        // Expands each image.
-                        while (i < img.length) {
-                            let myImg = img[i];
-
-                            getMeta(
-                                myImg.src,
-                                function(width, height) {
-                                    let imgWidth = width,
-                                        imgHeight = height;
-
-                                    // Applies expand styles only to lanscape images.
-                                    if (imgWidth > pageWidthPx && (imgWidth > imgHeight)) {
-                                        if (imgWidth > imgWideWidthPx) {
-                                            imgHeight = parseInt(imgWideWidthPx * imgHeight / imgWidth);
-                                            imgWidth = imgWideWidthPx;
-                                        }
-
-                                        // Defines the styles of the image.
-                                        let myImgStyle = "display:block;margin-left:50%;transform:translateX(-50%);max-width:100vw;";
-
-                                        // Sets the image attributes.
-                                        myImg.setAttribute("style", myImgStyle);
-                                        myImg.setAttribute("width", imgWidth);
-                                        myImg.setAttribute("height", imgHeight);
-                                    }
-                                }
-                            );
-
-                            i++;
-                        }
-                </script>
+                <script>window.addEventListener("load",imageWide);window.addEventListener("resize",imageWide);function getMeta(url,callback){var img=new Image();img.src=url;img.addEventListener("load",function(){callback(this.width,this.height)})}
+function imageWide(){var pageWidthEm=<?php echo $page_width_em; ?>,imgWideWidthEm=pageWidthEm+10,pageWidthPx=0,imgWideWidthPx=0,fontSizePx=0;const element=document.createElement('div');element.style.width='1rem';element.style.display='none';document.body.append(element);const widthMatch=window.getComputedStyle(element).getPropertyValue('width').match(/\d+/);element.remove();if(widthMatch&&widthMatch.length>=1){fontSizePx=parseInt(widthMatch[0])}
+if(fontSizePx>0){pageWidthPx=pageWidthEm*fontSizePx;imgWideWidthPx=imgWideWidthEm*fontSizePx}
+var img=document.getElementsByTagName("article")[0].getElementsByTagName("img"),i=0;while(i<img.length){let myImg=img[i];getMeta(myImg.src,function(width,height){let imgWidth=width,imgHeight=height,myImgStyle="";if(imgWidth>pageWidthPx&&imgWidth>imgHeight){if(imgWidth>imgWideWidthPx){imgHeight=parseInt(imgWideWidthPx*imgHeight/imgWidth);imgWidth=imgWideWidthPx}
+myImgStyle="display:block;margin-left:50%;transform:translateX(-50%);max-width:100vw;";myImg.setAttribute("style",myImgStyle);if(imgWidth){myImg.setAttribute("width",imgWidth)}
+if(imgHeight){myImg.setAttribute("height",imgHeight)}}});i++}}</script>
             <?php
         }
     }
---
+++ js/image-wide.js
@@ -0,0 +1,96 @@
+/**
+ * Enables wide images in posts.
+ *
+ * Note:
+ * This file is not directly loaded. Its content has been minimized
+ * and integrated into the _public.php file.
+ */
+
+window.addEventListener("load", imageWide);
+window.addEventListener("resize", imageWide);
+
+function getMeta(url, callback) {
+    var img     = new Image();
+        img.src = url;
+
+    img.addEventListener("load", function() {
+        callback(this.width, this.height);
+    });
+}
+
+function imageWide() {
+  var pageWidthEm    = <?php echo $page_width_em; ?>, // Yes, there is PHP.
+      imgWideWidthEm = pageWidthEm + 10,
+      pageWidthPx    = 0, // To set later.
+      imgWideWidthPx = 0, // To set later.
+      fontSizePx     = 0; // To set later.
+
+  /**
+   * Gets the font size defined by the browser.
+   *
+   * @link https://brokul.dev/detecting-the-default-browser-font-size-in-javascript
+   */
+  const element = document.createElement('div');
+
+  element.style.width   = '1rem';
+  element.style.display = 'none';
+
+  document.body.append(element);
+
+  const widthMatch = window.getComputedStyle(element).getPropertyValue('width').match(/\d+/);
+
+  element.remove();
+
+  // Sets the font size in px.
+  if (widthMatch && widthMatch.length >= 1) {
+      fontSizePx = parseInt(widthMatch[0]);
+  }
+
+  // If a font size is set, calculates page and image width in px.
+  if (fontSizePx > 0) {
+      pageWidthPx    = pageWidthEm * fontSizePx;
+      imgWideWidthPx = imgWideWidthEm * fontSizePx;
+  }
+
+  // Gets all images of the post.
+  var img = document.getElementsByTagName("article")[0].getElementsByTagName("img"),
+      i   = 0;
+
+  // Expands each image.
+  while (i < img.length) {
+      let myImg = img[i];
+
+      getMeta(
+          myImg.src,
+          function(width, height) {
+              let imgWidth   = width,
+                  imgHeight  = height,
+                  myImgStyle = "";
+
+              // Applies expand styles only to lanscape images.
+              if (imgWidth > pageWidthPx && imgWidth > imgHeight) {
+                  if (imgWidth > imgWideWidthPx) {
+                      imgHeight = parseInt(imgWideWidthPx * imgHeight / imgWidth);
+                      imgWidth  = imgWideWidthPx;
+                  }
+
+                  // Defines the styles of the image.
+                  myImgStyle = "display:block;margin-left:50%;transform:translateX(-50%);max-width:100vw;";
+
+                  // Sets the image attributes.
+                  myImg.setAttribute("style", myImgStyle);
+
+                  if (imgWidth) {
+                      myImg.setAttribute("width", imgWidth);
+                  }
+
+                  if (imgHeight) {
+                      myImg.setAttribute("height", imgHeight);
+                  }
+              }
+          }
+      );
+
+      i++;
+  }
+}
